05194700aee4c26305d59c776755d71d88198506
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-25 16:19:23 +0800

    更新合约代码

--- contracts/Wtf.sol
+++ contracts/Wtf.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.8.9;
 
 contract Wtf {
   string public _text = "Hello Web3!";
+  // bool public _b = true;
+  // uint256 public num = 123;
+  // address public addr = 0x7A58c0Be72BE218B41C608b7Fe7C5bB630736C71;
+  // address payable public addr2 = payable(addr);
+
+  // function hello() public pure returns (uint256) {
+  //   return 0;
+  // }
+
+  // function ppp() public returns (bool) {
+  //   return _b;
+  // }
 }
