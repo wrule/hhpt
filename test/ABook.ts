1fffc942a17ce219949a8aa382e5ffcdb6916282
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-26 15:38:46 +0800

    修正

--- test/ABook.ts
+++ test/ABook.ts
@@ -3,7 +3,7 @@ import { ethers } from 'hardhat';
 
 describe("ABook", function () {
   it("name", async function () {
-    const jimao = await ethers.deployContract('JIMAO');
+    // const jimao = await ethers.deployContract('JIMAO');
     const usdt = await ethers.deployContract('USDT');
     expect(1).to.equal(1);
   });
