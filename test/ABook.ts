1b599659c392ec166141f9bbc35e7ab578c7ed78
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-26 15:43:40 +0800

    name测试

--- test/ABook.ts
+++ test/ABook.ts
@@ -3,8 +3,15 @@ import { ethers } from 'hardhat';
 
 describe("ABook", function () {
   it("name", async function () {
-    const jimao = await ethers.deployContract('JIMAO');
-    const usdt = await ethers.deployContract('USDT');
+    const [jimao, usdt] = await Promise.all([
+      ethers.deployContract('JIMAO'),
+      ethers.deployContract('USDT'),
+    ]);
+    const abook = await ethers.deployContract('ABook', await Promise.all([
+      jimao.getAddress(),
+      usdt.getAddress(),
+    ]));
+    console.log(await abook.name());
     expect(1).to.equal(1);
   });
 });
