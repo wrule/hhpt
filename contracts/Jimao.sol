b0ff88d14f40aa4f18af8569120f5cbaed995b0b
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-26 15:33:17 +0800

    jimao

---
+++ contracts/Jimao.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Jimao is ERC20 {
+  constructor() ERC20("Jimao Token", "JIMAO") {
+    _mint(address(this), 1e8 * 1e10);
+  }
+
+  function decimals() public pure override returns (uint8) {
+    return 10;
+  }
+
+  function airdrop() external {
+    _transfer(address(this), msg.sender, 10 * 1e6);
+  }
+}
--- contracts/USDT.sol
+++ contracts/USDT.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract JimaoToken is ERC20 {
-  constructor() ERC20("JimaoToken", "JMT") {
+contract USDT is ERC20 {
+  constructor() ERC20("USDT", "USDT") {
     _mint(address(this), 1e3 * 1e8 * 1e6);
   }
 
-  function decimals() public pure virtual override returns (uint8) {
+  function decimals() public pure override returns (uint8) {
     return 6;
   }
 
-  function airdrop() public {
+  function airdrop() external {
     _transfer(address(this), msg.sender, 10 * 1e6);
   }
 }
