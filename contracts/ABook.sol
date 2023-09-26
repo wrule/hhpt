bd75f495288fd05cf1f73bd0d3a31b5b6724b46a
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-26 15:26:25 +0800

    abook的名称

---
+++ contracts/ABook.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ABook {
+  ERC20 baseToken;
+  ERC20 quotaToken;
+
+  constructor(
+    address baseAddr,
+    address quoteAddr
+  ) {
+    baseToken = ERC20(baseAddr);
+    quotaToken = ERC20(quoteAddr);
+  }
+
+  function name() public view returns (string memory) {
+    return string(abi.encodePacked(baseToken.name(), quotaToken.name()));
+  }
+
+  function LimitBuy() public {
+
+  }
+
+  function LimitSell() public {
+
+  }
+
+  function MarketBuy() public {
+
+  }
+
+  function MarketSell() public {
+
+  }
+}
