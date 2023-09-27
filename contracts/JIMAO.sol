5718805894c86d0f70934784a902497be0ecedfe
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-27 15:05:03 +0800

    使用Ownable

--- contracts/JIMAO.sol
+++ contracts/JIMAO.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract JIMAO is ERC20 {
+contract JIMAO is ERC20, Ownable {
   constructor() ERC20("Jimao Token", "JIMAO") {
     _mint(address(this), 1e3 * 1e8 * 1e10);
   }
@@ -12,7 +13,7 @@ contract JIMAO is ERC20 {
     return 10;
   }
 
-  function airdrop() external {
+  function airdrop() external onlyOwner {
     _transfer(address(this), msg.sender, 1e6 * 1e10);
   }
 }
