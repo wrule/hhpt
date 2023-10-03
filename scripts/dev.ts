fb1210eb9a728f605312565af988798dcc90f033
jimao <jimao@jimaodeMBP.lan>
2023-10-03 13:11:43 +0800

    测试脚本代码

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -3,7 +3,7 @@ import { ethers } from 'hardhat';
 async function main() {
   const signer = (await ethers.getSigners())[0];
   const jimao = await ethers.deployContract('JIMAO');
-  const a = await jimao.depositETH();
+  const a = await jimao.myDeposits(1, 10);
   console.log(a);
 }
 
