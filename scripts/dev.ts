fe317e5c30e3491e6b22c125e18e90e3e7369922
jimao <jimao@jimaodeMBP.lan>
2023-10-03 13:16:34 +0800

    测试脚本

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -3,7 +3,21 @@ import { ethers } from 'hardhat';
 async function main() {
   const signer = (await ethers.getSigners())[0];
   const jimao = await ethers.deployContract('JIMAO');
-  const a = await jimao.myDeposits(1, 10);
+
+  await jimao.depositETH({ value: ethers.parseEther('0.1') });
+  await jimao.depositETH({ value: ethers.parseEther('0.2') });
+  await jimao.depositETH({ value: ethers.parseEther('0.3') });
+  await jimao.depositETH({ value: ethers.parseEther('0.4') });
+  await jimao.depositETH({ value: ethers.parseEther('0.5') });
+  await jimao.depositETH({ value: ethers.parseEther('0.6') });
+  await jimao.depositETH({ value: ethers.parseEther('0.7') });
+  await jimao.depositETH({ value: ethers.parseEther('0.8') });
+  await jimao.depositETH({ value: ethers.parseEther('0.9') });
+  await jimao.depositETH({ value: ethers.parseEther('0.11') });
+  await jimao.depositETH({ value: ethers.parseEther('0.22') });
+  await jimao.depositETH({ value: ethers.parseEther('0.33') });
+
+  const a = await jimao.myDeposits(2, 10);
   console.log(a);
 }
 
