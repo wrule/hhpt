ed859781fe9416dc53799217825453e84c0da4ea
jimao <jimao@jimaodeMBP.lan>
2023-10-03 12:54:39 +0800

    分页逻辑

--- contracts/JIMAO.sol
+++ contracts/JIMAO.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+struct Deposit {
+  bool isValid;
+  uint256 amount;
+  int64 withdrawTime;
+  address contractAddress;
+}
+
 contract JIMAO is ERC20, Ownable {
   constructor() ERC20("Jimao Token", "JIMAO") {
     _mint(address(this), 1e3 * 1e8 * 1e10);
@@ -15,5 +22,37 @@ contract JIMAO is ERC20, Ownable {
 
   function airdrop() external onlyOwner {
     _transfer(address(this), msg.sender, 1e6 * 1e10);
+  }
+
+  mapping(address => Deposit[]) public db;
+
+  function depositETH() external payable {
+    require(msg.value > 0, 'error');
+    Deposit memory newDeposit;
+    newDeposit.isValid = true;
+    newDeposit.amount = msg.value;
+    newDeposit.withdrawTime = 0;
+    newDeposit.contractAddress = address(0);
+    db[msg.sender].push(newDeposit);
+  }
+
+  function myDeposits(
+    uint pageNum,
+    uint pageSize
+  ) view external returns (Deposit[] memory) {
+    Deposit[] memory deposits = db[msg.sender];
+    uint total = deposits.length;
+    uint lastPageNum = total / pageSize + (total % pageSize == 0 ? 0 : 1);
+    if (lastPageNum == 0) lastPageNum = 1;
+    if (pageNum > lastPageNum) pageNum = lastPageNum;
+
+    uint startIndex = (pageNum - 1) * pageSize;
+    uint endIndex = startIndex + pageSize;
+    if (endIndex > total) endIndex = total;
+    Deposit[] memory result = new Deposit[](endIndex - startIndex);
+    for (uint i = startIndex; i < endIndex; ++i) {
+      result[i - startIndex] = deposits[startIndex];
+    }
+    return result;
   }
 }
--- scripts/dev.ts
+++ scripts/dev.ts
@@ -2,16 +2,9 @@ import { ethers } from 'hardhat';
 
 async function main() {
   const signer = (await ethers.getSigners())[0];
-  const token = await ethers.deployContract('JimaoToken');
-  const a = await token.totalSupply();
+  const jimao = await ethers.deployContract('JIMAO');
+  const a = await jimao.depositETH();
   console.log(a);
-  const b = await token.decimals();
-  console.log(b);
-  const tx = await token.airdrop();
-  const tr = await tx.wait(2);
-  console.log(tx);
-  const c = await token.balanceOf(signer.address);
-  console.log(c);
 }
 
 main();
